4fe244213e6df35090201dec65916630b6daddfc
gurustump <gurustump@gurustump.com>
2016-09-05 04:44:37 -0700

    fixing accordion on equipment page mobile so it won't get cut off at current size, adding styles for single blog posts

--- post-formats/format.php
+++ post-formats/format.php
@@ -14,58 +14,64 @@
                 */
               ?>
 
-              <article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting">
+				<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting">
 
-                <header class="article-header entry-header">
+					<header class="article-header entry-header">
+						<?php if (has_post_thumbnail) { ?>
+						<div class="post-thumb-container">
+							<?php the_post_thumbnail('movie-thumb'); ?>
+						</div>
+						<?php } ?>
+						<div class="header-text-container">
+							<h1 class="entry-title single-title" itemprop="headline" rel="bookmark"><?php the_title(); ?></h1>
 
-                  <h1 class="entry-title single-title" itemprop="headline" rel="bookmark"><?php the_title(); ?></h1>
+							<p class="byline entry-meta vcard">
+							<?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+							   /* the time the post was published */
+							   '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+							   /* the author of the post */
+							   '<span class="by">'.__( 'by', 'bonestheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+							); ?>
+							</p>
+							<div class="post-categories">
+								<?php printf( __( 'Filed under', 'bonestheme' ).': %1$s', get_the_category_list(', ') ); ?>
+							</div>
+						</div>
 
-                  <p class="byline entry-meta vcard">
+					</header> <?php // end article header ?>
 
-                    <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                       /* the time the post was published */
-                       '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       /* the author of the post */
-                       '<span class="by">'.__( 'by', 'bonestheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    ); ?>
+					<section class="entry-content cf" itemprop="articleBody">
+						<?php
+							// the content (pretty self explanatory huh)
+							the_content();
 
-                  </p>
+							/*
+							 * Link Pages is used in case you have posts that are set to break into
+							 * multiple pages. You can remove this if you don't plan on doing that.
+							 *
+							 * Also, breaking content up into multiple pages is a horrible experience,
+							 * so don't do it. While there are SOME edge cases where this is useful, it's
+							 * mostly used for people to get more ad views. It's up to you but if you want
+							 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
+							 *
+							 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
+							 *
+							*/
+							wp_link_pages( array(
+							  'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
+							  'after'       => '</div>',
+							  'link_before' => '<span>',
+							  'link_after'  => '</span>',
+							) );
+						?>
+					</section> <?php // end article section ?>
 
-                </header> <?php // end article header ?>
+                <?php /* <footer class="article-footer">
 
-                <section class="entry-content cf" itemprop="articleBody">
-                  <?php
-                    // the content (pretty self explanatory huh)
-                    the_content();
-
-                    /*
-                     * Link Pages is used in case you have posts that are set to break into
-                     * multiple pages. You can remove this if you don't plan on doing that.
-                     *
-                     * Also, breaking content up into multiple pages is a horrible experience,
-                     * so don't do it. While there are SOME edge cases where this is useful, it's
-                     * mostly used for people to get more ad views. It's up to you but if you want
-                     * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-                     *
-                     * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-                     *
-                    */
-                    wp_link_pages( array(
-                      'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-                      'after'       => '</div>',
-                      'link_before' => '<span>',
-                      'link_after'  => '</span>',
-                    ) );
-                  ?>
-                </section> <?php // end article section ?>
-
-                <footer class="article-footer">
-
-                  <?php printf( __( 'filed under', 'bonestheme' ).': %1$s', get_the_category_list(', ') ); ?>
 
                   <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 
-                </footer> <?php // end article footer ?>
+                </footer> <?php // end article footer ?> */ ?>
 
                 <?php //comments_template(); ?>
 
