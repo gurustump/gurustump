dda78b16ad9a4b9a4c1b00563fce77a7fc54abd9
gurustump <gurustump@gurustump.com>
2017-03-24 19:34:12 -0700

    making 404 template look better, fixing typo in mixins.scss, and adding css for a CMB2 type that I'm not currently using in the admin

--- 404.php
+++ 404.php
@@ -10,28 +10,24 @@
 
 							<header class="article-header">
 
-								<h1><?php _e( 'Epic 404 - Article Not Found', 'bonestheme' ); ?></h1>
+								<h1><?php _e( '404 - Article Not Found', 'bonestheme' ); ?></h1>
 
 							</header>
 
 							<section class="entry-content">
 
-								<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?></p>
+								<p><?php _e( 'The item you were looking for was not found, but maybe try looking again:', 'bonestheme' ); ?></p>
+
+								<p><?php get_search_form(); ?></p>
 
 							</section>
-
-							<section class="search">
-
-									<p><?php get_search_form(); ?></p>
-
-							</section>
-
+<?php /*
 							<footer class="article-footer">
 
 									<p><?php _e( 'This is the 404.php template.', 'bonestheme' ); ?></p>
 
 							</footer>
-
+*/ ?>
 						</article>
 
 					</main>
